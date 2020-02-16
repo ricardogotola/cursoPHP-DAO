62ab9faef8de25efdc362804b9754860ba2d2719
Ricardo Gotola <55255181+WakeUP-Solucoes@users.noreply.github.com>
2020-02-16 04:39:21 -0300

    consulta por $ID funcionando

--- index.php
+++ index.php
@@ -2,8 +2,13 @@
 
 require_once ("config.php");
 
-$sql = new Sql();
+$root = new Usuario();
+$root->loadById(3);
+echo $root;
 
-$usuarios = $sql->select("SELECT * FROM tb_usuarios");
 
-echo json_encode($usuarios);
+
+
+//$sql = new Sql();
+//$usuarios = $sql->select("SELECT * FROM tb_usuarios");
+//echo json_encode($usuarios);
